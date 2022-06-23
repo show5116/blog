8e310cb7838e43cd78e1126c0360890cf9b80baf
youyeongjin <show5116@naver.com>
2022-06-23 14:25:16 +0900

    FEAT : add ThemeProvider

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-advanced-sitemap`,
+    `gatsby-plugin-theme-ui`,
     `gatsby-plugin-typescript`,
     {
       resolve: 'gatsby-plugin-robots-txt',
--- wrap-with-provider.tsx
+++ wrap-with-provider.tsx
@@ -3,12 +3,17 @@ import { Provider } from 'react-redux';
 
 import createStore from './src/state/createStore';
 import StyledThemeProvider from './src/components/StyledThemeProvider';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 export default ({ element }) => {
   const store = createStore();
+  const sheet = new ServerStyleSheet();
+
   return (
     <Provider store={store}>
-      <StyledThemeProvider>{element}</StyledThemeProvider>
+      <StyleSheetManager sheet={sheet.instance}>
+        <StyledThemeProvider>{element}</StyledThemeProvider>
+      </StyleSheetManager>
     </Provider>
   );
 };
